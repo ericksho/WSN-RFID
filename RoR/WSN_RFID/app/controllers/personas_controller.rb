fbb8cfca6baa45e95ba1ecae9b5cd3ed322a0dbd
Ignacio Carmach <icarmach@gmail.com>
2014-06-22 19:41:14 -0400

    vista personas

--- RoR/WSN_RFID/app/controllers/personas_controller.rb
+++ RoR/WSN_RFID/app/controllers/personas_controller.rb
@@ -30,7 +30,7 @@ class PersonasController < ApplicationController
 
     respond_to do |format|
       if @persona.save
-        format.html { redirect_to personas_path, notice: 'Persona was successfully created.' }
+        format.html { redirect_to personas_path, notice: 'La persona fue creada con éxito.' }
         format.json { render :show, status: :created, location: @persona }
       else
         format.html { render :new }
@@ -44,7 +44,7 @@ class PersonasController < ApplicationController
   def update
     respond_to do |format|
       if @persona.update(persona_params)
-        format.html { redirect_to @persona, notice: 'Persona was successfully updated.' }
+        format.html { redirect_to @persona, notice: 'La persona fue actualizada.' }
         format.json { render :show, status: :ok, location: @persona }
       else
         format.html { render :edit }
@@ -58,7 +58,7 @@ class PersonasController < ApplicationController
   def destroy
     @persona.destroy
     respond_to do |format|
-      format.html { redirect_to personas_url, notice: 'Persona was successfully destroyed.' }
+      format.html { redirect_to personas_url, notice: 'La persona fue eliminada.' }
       format.json { head :no_content }
     end
   end
