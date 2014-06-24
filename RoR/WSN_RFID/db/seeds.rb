eade2529035d55e6de9e1616084cde072b11ff70
Esteban <enpinone@uc.cl>
2014-06-23 23:43:33 -0400

    arregladas seeds

--- RoR/WSN_RFID/db/seeds.rb
+++ RoR/WSN_RFID/db/seeds.rb
@@ -6,11 +6,13 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+p = Permiso.create(titulo: "Administrador", acceso_articulo: true, acceso_lectura: true, acceso_lector: true, acceso_alerta: true, acceso_permiso: true, acceso_persona: true, acceso_usuario: true, acceso_vehiculo: true)
+u1 = Usuario.create(nombre: "Esteban", apellido: "Piñones", email: "cabasp@gmail.com", password: "123123123", password_confirmation: "123123123", cargo: "Administrador")
+u2 = Usuario.create(nombre: "Ignacio", apellido: "Carmach", email: "icarmach@gmail.com", password: "123123123", password_confirmation: "123123123", cargo: "Administrador")
+u3 = Usuario.create(nombre: "Erick", apellido: "Svec", email: "ericksho@gmail.com", password: "123123123", password_confirmation: "123123123", cargo: "Administrador")
+u4 = Usuario.create(nombre: "Tomás", apellido: "Martinez", email: "t.martinezg@gmail.com", password: "123123123", password_confirmation: "123123123", cargo: "Administrador")
 
-u1 = Usuario.create(nombre: "Esteban", apellido: "Piñones", email: "cabasp@gmail.com", password: "123123123", password_confirmation: "123123123")
-Usuario.create(nombre: "Ignacio", apellido: "Carmach", email: "icarmach@gmail.com", password: "123123123", password_confirmation: "123123123")
-Usuario.create(nombre: "Erick", apellido: "Svec", email: "ericksho@gmail.com", password: "123123123", password_confirmation: "123123123")
-Usuario.create(nombre: "Tomás", apellido: "Martinez", email: "t.martinezg@gmail.com", password: "123123123", password_confirmation: "123123123")
-
-p = Permiso.create(titulo: "Administrador", acceso_articulo: false, acceso_lectura: true, acceso_lector: true, acceso_alerta: true, acceso_permiso: true, acceso_persona: true, acceso_usuario: true, acceso_vehiculo: true)
 u1.permisos << p
+u2.permisos << p
+u3.permisos << p
+u4.permisos << p
